abbec52a6520d6c1ec9627501fb6234a91a92d01
mckjzhangxk <mckj_zhangxk@163.com>
2018-08-17 14:04:06 +0800

    sequence predict

---
+++ c++/week2/2.7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+enum  colors {RED,GREEM,BLUE};
+/*
+	use of enum
+*/
+
+int main(int argc,char **argv){
+	colors c;
+	cout<<"color is:"<<c<<endl;
+	return 0;
+}
+
