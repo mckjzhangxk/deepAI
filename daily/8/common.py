91ef46aa9d6eda96986ed2ecb7718ccdabb6305f
zhangxk <mckj_zhangxk@163.com>
2018-09-12 01:22:08 +0800

    xxx

--- daily/8/common.py
+++ daily/8/common.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import itertools
+from tensorflow.examples.tutorials.mnist import input_data
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
@@ -34,3 +35,48 @@ def plot_confusion_matrix(cm, classes,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+    
+mnist_data_path='../AI_database/mnist/MNIST_DATA'
+def imshow(X,Y=None,classes=None):
+    '''
+        show Batch of image in grids sqrt(h) x sqrt(w)
+        X is a numpy array,size (m,h,w,c)
+        Y is a numpy array,size (m,#classes)
+    '''
+    m=X.shape[0]
+    gridSize=int(m**0.5)
+    for i in range(0,gridSize):
+        for j in range(0,gridSize):
+            _idx=i*gridSize+j
+            im=X[_idx]
+            plt.subplot(gridSize,gridSize,_idx+1)
+            plt.axis('off')
+            plt.imshow(im)
+            if Y is not None:
+                label=classes[np.argmax(Y[_idx])]
+                plt.title(label)
+
+def load_dataset(flaten=False,one_hot=True):
+    def _make_one_hot(d,C=10):
+        return (np.arange(C)==d[:,None]).astype(np.int32)
+
+    mnist=input_data.read_data_sets(mnist_data_path)
+    X_train,Y_train=mnist.train.images,mnist.train.labels
+    X_test,Y_test=mnist.test.images,mnist.test.labels
+
+    if flaten==False:
+        X_train=X_train.reshape((-1,28,28,1))
+        X_test = X_test.reshape((-1, 28, 28,1))
+    if one_hot:
+        Y_train = _make_one_hot(Y_train)
+        Y_test=_make_one_hot(Y_test)
+
+
+    print('\n-------------------------------------------------------------------------')
+    print('load %d train Example,%d Test Example'%(X_train.shape[0],X_test.shape[0]))
+    print('Train Images  Shape:'+str(X_train.shape))
+    print('Train Labels  Shape:' + str(Y_train.shape))
+    print('Test  Images  Shape:'+str(X_test.shape))
+    print('Test  Labels  Shape:' + str(Y_test.shape))
+    print('-------------------------------------------------------------------------')
+    return (X_train,Y_train,X_test,Y_test)
