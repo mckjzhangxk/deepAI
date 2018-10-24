a3a3f52121ee5e3d5eb7d47e13d491baea9e7583
zhangxk <mckj_zhangxk@163.com>
2018-10-24 09:46:37 +0800

    xx

--- daily/8/ml_zou/class2.py
+++ daily/8/ml_zou/class2.py
@@ -6,30 +6,33 @@ import matplotlib as mpl
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-def drawNormDistribution(fig,X,Y,Z,index):
-    ax=fig.add_subplot(index,projection='3d')
+def drawNormDistribution(X,Y,Z,msg):
+    fig=plt.figure()
+    ax=fig.add_subplot(111,projection='3d')
     ax.plot_surface(X,Y,Z,rstride=2,cstride=2,cmap=cm.Accent)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
+    plt.title(msg)
+    fig.show()
 X,Y=np.mgrid[-6:6:100j,-6:6:100j]
-fig=plt.figure()
+
 #标准正太分布
 Z=(0.5/np.pi)*np.exp(-0.5*(X**2+Y**2))
-drawNormDistribution(fig,X,Y,Z,221)
-plt.title(r'$ \mu=0,\ \sigma=I$')
+drawNormDistribution(X,Y,Z,r'$ \mu=0,\ \sigma=I$')
 
 # 正太分布,u=1,2
 Z=(0.5/np.pi)*np.exp(-0.5*((X-1)**2+(Y-2)**2))
-drawNormDistribution(fig,X,Y,Z,222)
-plt.title(r'$\mu=[1,2],\ \sigma=I$')
+drawNormDistribution(X,Y,Z,r'$\mu=[1,2],\ \sigma=I$')
+
 # cov=[3 0,0 1]
 Z=(0.5/np.pi/3)*np.exp(-((X**2)/3+Y**2))
-drawNormDistribution(fig,X,Y,Z,223)
-plt.title(r'$ \mu=0,\ \sigma=[3,0,0,1]$')
+drawNormDistribution(X,Y,Z,r'$ \mu=0,\ \sigma=[3,0,0,1]$')
+
 
 # cov=[1 0.5,0.5 1]
+# cov=[1 0.5,0.5 1],inv(cov)=array([[ 1.33333333, -0.66666667],
+#        [-0.66666667,  1.33333333]]),所以副向关
 Z=(0.5/np.pi/0.75)*np.exp(-(X**2+Y**2+X*Y))
-drawNormDistribution(fig,X,Y,Z,224)
-plt.title(r'$ \mu=0,\ \sigma=[1,.5,.5,1]$')
-fig.show()
+drawNormDistribution(X,Y,Z,r'$ \mu=0,\ \sigma=[1,.5,.5,1]$')
+
