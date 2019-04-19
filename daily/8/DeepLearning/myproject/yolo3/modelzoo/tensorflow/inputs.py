d1480c07eb7a066232ba30ef0ec1bac712f5ebb5
zhangxk <mckj_zhangxk@163.com>
2019-04-19 16:08:14 +0800

    ds

--- daily/8/DeepLearning/myproject/yolo3/modelzoo/tensorflow/inputs.py
+++ daily/8/DeepLearning/myproject/yolo3/modelzoo/tensorflow/inputs.py
@@ -1,9 +1,14 @@
 import cv2
 import os
 import numpy as np
+from tensorpack.dataflow import imgaug
 
 def dataIterator(imageprefix,valfile,batchsize):
-
+    augmentors = [
+        imgaug.ResizeShortestEdge(256, interp=cv2.INTER_CUBIC),
+        imgaug.CenterCrop((224, 224)),
+    ]
+    aug = imgaug.AugmentorList(augmentors)
     with open(valfile) as fs:
         while True:
             lines=[]
@@ -11,34 +16,20 @@ def dataIterator(imageprefix,valfile,batchsize):
                 l=fs.readline().strip('\n')
                 if l=='':break
                 lines.append(l)
+            if len(lines)==0:return
             Xs,Ys=[],[]
             for line in lines:
                 sps=line.split(' ')
                 imagepath=os.path.join(imageprefix,sps[0])
-                if not os.path.exists(imagepath):
-                    continue
                 X=cv2.imread(imagepath)
+                assert X is not None ,imagepath
                 Y=int(sps[1])
 
-                Xs.append(X)
+                Xs.append(aug.augment(X))
                 Ys.append(Y)
             Xs=np.array(Xs)
             Ys=np.array(Ys)
             yield Xs, Ys
-
-# [103.939, 116.779, 123.68]
-# iterator=dataIterator('/home/zxk/AI/ILSVRC2012/ILSVRC2012_img_val','/home/zxk/AI/ILSVRC2012/val.txt',128,transfer)
-# db=DataIterator('/home/zxk/AI/ILSVRC2012/ILSVRC2012_img_val',
-#                 '/home/zxk/AI/ILSVRC2012/val.txt',128,
-#                 '/home/zxk/AI/ILSVRC2012/ilsvrc_2012_mean.npy')
-
-# cnt=0
-#
-# for x,y in tqdm(db):pass
-    # cnt+=len(x)
-    # print(cnt)
-
-
 class DataIterator():
     def __init__(self,imageprefix,valfile,batchsize):
         self.prefix=imageprefix
@@ -48,3 +39,17 @@ class DataIterator():
     def __iter__(self):
         return dataIterator(self.prefix,self.valfile,self.batchsize)
     def __len__(self):return self.examples//self.batchsize
+
+# from tqdm import tqdm
+# # # [103.939, 116.779, 123.68]
+# # # iterator=dataIterator('/home/zxk/AI/ILSVRC2012/ILSVRC2012_img_val','/home/zxk/AI/ILSVRC2012/val.txt',128,transfer)
+# db=DataIterator('/home/zxk/AI/ILSVRC2012/ILSVRC2012_img_val',
+#                 '/home/zxk/AI/ILSVRC2012/val1.txt',4)
+# cnt=0
+# for x,y in tqdm(db):
+#     print(x.shape)
+#     cnt+=len(x)
+# print(cnt)
+
+
+
