73e516a8481d282dafc7b2ad03c33f1f209e1649
mckj_zhangxk <mckj_zhangxk@163.com>
2019-08-19 16:41:02 +0800

    l

--- daily/8/DeepLearning/myproject/videoAnalysis/search/pq.py
+++ daily/8/DeepLearning/myproject/videoAnalysis/search/pq.py
@@ -1,5 +1,8 @@
 import numpy as  np
 from sklearn.cluster import KMeans
+from threading import Thread
+
+
 def quanizer(X,codebook):
     '''
     
@@ -37,28 +40,47 @@ def quanizer(X,codebook):
     ret=np.argmin(dist,-1)
     return ret
 
+class MyWorker(Thread):
+    def __init__(self,id,data,codebook,maxIters=300):
+        super().__init__(name=str(id))
+        self.model=KMeans(z,n_jobs=1,n_init=1,verbose=0,max_iter=maxIters)
+        self.d=data
+        self.id=id
+        self.codebook=codebook
 
-def createCodeBook(X,m=64,z=256,outpath='codebook.npy',cpus=1,max_iter=300):
+    def run(self):
+        self.model.fit(self.d)
+        self.codebook[self.id]=self.model.cluster_centers_
+def createCodeBook(X,m=64,z=256,outpath='codebook.npy',cpus=1,max_iter=300,start=0):
     import tqdm
     d=X.shape[-1]
     assert d%m==0,"m必须是d 的因数"
     dreduce=d//m
     X=X.reshape((-1,m,dreduce))
 
-    codebook=[]
+    codebook=[0]*(m-start)
+    threads=[]
 
-    for im in tqdm.tqdm(range(m)):
+    import multiprocessing
+    cpus=multiprocessing.cpu_count()
 
-        model=KMeans(z,n_jobs=cpus,verbose=1,max_iter=max_iter)
-        model.fit(X[:,im,:])
-        codebook.append(model.cluster_centers_)
-    codebook=np.array(codebook)
+    for im in tqdm.tqdm(range(start,m)):
 
-    assert codebook.shape==(m,z,dreduce)
+        t = MyWorker(im, X[:, im, :], codebook, max_iter)
+        threads.append(t)
+        t.start()
+        if im%cpus==0 and im>0:
+            for t in threads:
+                t.join()
+            threads=[]
+    for t in threads:
+        t.join()
+    codebook = np.array(codebook)
+    assert codebook.shape==(m-start,z,dreduce)
     np.save(outpath,codebook)
 
 if __name__ == '__main__':
-    N=300
+    N=3000
     d=512
     m=64
     z=256
@@ -71,10 +93,11 @@ if __name__ == '__main__':
     #
     # assert r.shape==(N,m)
 
-    import bcolz
+    # import bcolz
 
-    x=bcolz.open('/home/zxk/桌面/dump/feature','r')
-    x=x[:]
-    print(x.shape)
-    # x=np.random.rand(N,d)
+    # x=bcolz.open('/home/zxk/桌面/dump/feature','r')
+    # x=x[:]
+    # print(x.shape)
+    x=np.random.rand(N,d).astype(np.float16)
+    print(x.dtype)
     createCodeBook(x,m=m,z=z,cpus=-1)
