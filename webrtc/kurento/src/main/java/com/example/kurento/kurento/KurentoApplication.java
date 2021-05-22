9141503f06ffe1b57268c683de97ab6979de5a3f
zhangxk <mckj_zhangxk@163.com>
2021-05-22 09:17:35 +0800

    1

---
+++ webrtc/kurento/src/main/java/com/example/kurento/kurento/KurentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurento.kurento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurentoApplication.class, args);
+	}
+
+}
